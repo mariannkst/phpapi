bad6eb3fd507a849f33812d4e38111320da3a928
mariannK <karsz@DESKTOP-O3C7C87>
2021-03-03 17:20:46 +0100

    Get users

---
+++ api-get-users.php
@@ -0,0 +1,41 @@
+<?php
+/*
+if (!isset($_SERVER['PHP_AUTH_USER'])) {
+    header('WWW-Authenticate: Basic realm="My Realm"');
+    header('HTTP/1.0 401 Unauthorized');
+    echo 'Text to send if user hits Cancel button';
+    exit;
+} else {
+    echo "<p>Hello {$_SERVER['PHP_AUTH_USER']}.</p>";
+    echo "<p>You entered {$_SERVER['PHP_AUTH_PW']} as your password.</p>";
+}*/
+
+
+/*
+header("Content-Type:application/json");
+*/
+require_once 'database.php';
+
+try{
+    $sQuery = $db->prepare('SELECT * FROM users');
+    $sQuery->execute();
+    // Array 
+    $aUsers = $sQuery->fetchAll();
+
+    if (empty($aUsers)) {
+
+        echo '{"statusCode":"'. http_response_code() .'", "message": "There are no users"}';
+        exit();
+
+    } else if (!empty($aUsers)){
+        echo json_encode($aUsers);
+    }
+
+
+}catch(PDOException $ex){
+
+    http_response_code(500);
+    echo '{"statusCode":"'. http_response_code() .'", "message": "Internal server error"}';
+    exit();
+
+}
---
+++ database.php
@@ -0,0 +1,18 @@
+<?php
+
+try{ 
+    $sUserName = 'root';
+    $sPassword = '';
+    $sConnection = "mysql:host=localhost; dbname=app; charset=utf8mb4";
+
+    $aOptions = array(
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    );
+    $db = new PDO( $sConnection, $sUserName, $sPassword, $aOptions );
+
+}catch( PDOException $e){
+    http_response_code(503);
+    echo '{"statusCode":"'.http_response_code().'","message":"Service unavailable"}';
+    exit();
+}
